da872651d81aaa6be0d907cb3259a34634966356
Bhagyashri2216 <bhagyashrif1995@gmail.com>
2023-05-22 14:55:22 +0530

    solved qu 10

---
+++ JS Question/Question10/question.js
@@ -0,0 +1,50 @@
+// 10. Figure out the result of the following comparison expression first without using
+//  console.log(). After you decide the result confirm it using console.log()
+
+
+
+// 4 > 3  true
+console.log(4 > 3);
+
+// 4 >= 3  true
+console.log(4 >= 3)
+
+// 4 < 3  false
+console.log(4 < 3)
+
+// 4 <= 3  false
+console.log( 4 <= 3)
+
+// 4 == 4  true
+console.log( 4 == 4)
+
+//  4 === 4  true
+console.log( 4 === 4)
+
+//  4 != 4  false
+console.log( 4 != 4)
+
+//  4 !== 4  false
+console.log( 4 !== 4)
+
+// 4 != '4'  false
+console.log( 4 != '4')
+
+// 4 == '4'  true
+console.log( 4 == '4')
+
+//  4 === '4'  false
+console.log( 4 === '4')
+
+
+
+// Q. Find the length of python and jargon and make a falsy comparison statement
+let python = "python"
+let jargon = "jargon"
+
+console.log(python.length);//6
+console.log(jargon.length);//6
+
+if (python.length == jargon.length) {
+    console.log(`Length of ${python} & ${jargon} is : ${python.length}`);
+}//Length of python & jargon is : 6
