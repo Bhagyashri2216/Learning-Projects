21a79942732c36b4ad5c6d307125636f20c56b7e
Bhagyashri2216 <bhagyashrif1995@gmail.com>
2023-05-22 12:50:56 +0530

    solvedqu 04

--- JS Question/Question04/question04.js
+++ JS Question/Question04/question04.js
@@ -1,2 +1,8 @@
 // 04. Declare a varibale and assign string value to it and check if 
 // the string contains a word Script using includes() method.
+let text="I am a web developer";
+let result=text.includes("Script");
+console.log(result)//false
+let text1="Hello Script";
+let result1=text1.includes("Script");
+console.log(result1)//true
