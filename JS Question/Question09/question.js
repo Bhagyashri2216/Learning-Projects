1b8c28994b2880f7907361db9015aa342a295700
Bhagyashri2216 <bhagyashrif1995@gmail.com>
2023-05-22 20:26:53 +0530

    solved qu 9

---
+++ JS Question/Question09/question.js
@@ -0,0 +1,50 @@
+// 09. Boolean value is either true or false.
+//     - Write three JavaScript statement example which provide truthy value.
+//     - Write three JavaScript statement example which provide falsy value.
+console.log(Boolean(null));
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+console.log(Boolean(""));
+console.log(Boolean("A"));
+
+if (null) {
+    console.log("if block executed");
+}
+else {
+    console.log("else block executed");
+}
+
+if (0) {
+    console.log("if block executed");
+}
+else {
+    console.log("else block executed");
+}
+
+if (undefined) {
+    console.log("if block executed");
+}
+else {
+    console.log("else block executed");
+}
+
+if (NaN) {
+    console.log("if block executed");
+}
+else {
+    console.log("else block executed");
+}
+
+if ("") {
+    console.log("if block executed");
+}
+else {
+    console.log("else block executed");
+}
+if ("A") {
+    console.log("if block executed");
+}
+else {
+    console.log("else block executed");
+}
