485f5be6e1756285befbd4d3b94151fc3714d25c
Bhagyashri2216 <bhagyashrif1995@gmail.com>
2023-05-22 12:25:14 +0530

    solved question02

---
+++ JS Question/Question02/question02.js
@@ -0,0 +1,11 @@
+// 02. Declare variables to store your first name, last name, marital status, 
+// country and age and display them using interploation method.
+
+let Fname="Bhagyashri"
+let Lname="Fasale"
+let Mstatus="Unmarried"
+let country="India"
+let age=27
+
+console.log(`My name is ${Fname} ${Lname} aged ${age} I am ${Mstatus} belong to ${country}`)
+//My name is Bhagyashri Fasale aged 27 I am Unmarried belong to India
