1d5f3a29318c188b90adc0c235f2ef8e69ca1470
Bhagyashri2216 <bhagyashrif1995@gmail.com>
2023-05-22 13:57:41 +0530

    solved qu 06

---
+++ JS Question/Question06/question.js
@@ -0,0 +1,15 @@
+// 06. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' 
+// split the string at the comma and change it to an array.
+
+let companyname="Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+result=companyname.split(",")
+console.log(result)
+// [
+//     'Facebook',
+//     ' Google',
+//     ' Microsoft',
+//     ' Apple',
+//     ' IBM',
+//     ' Oracle',
+//     ' Amazon'\
+// ]
