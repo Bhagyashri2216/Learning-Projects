e1da81da6696a91029b12e11388001c9f6e5ed66
Bhagyashri2216 <bhagyashrif1995@gmail.com>
2023-05-22 12:05:50 +0530

    solved question

--- JS Question/Question01/question01.js
+++ JS Question/Question01/question01.js
@@ -1,2 +1,10 @@
 // 01. Create a variable.js file and declare variables and assign string, boolean,
 // undefined and null data types, Display all the value with their data type.
+let a = "Bhagyashri"
+let b = true
+let c = undefined
+let d = null
+console.log("Type of variable a is: ", typeof a)
+console.log("Type of variable b is: ", typeof b)
+console.log("Type of variable c is: ", typeof c)
+console.log("Type of variable d is: ", typeof d)
