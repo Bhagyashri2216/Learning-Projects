f47d81ecb3f1d6d331b117c0c2b6f8297233aa67
Bhagyashri2216 <bhagyashrif1995@gmail.com>
2023-05-22 12:41:15 +0530

    solved qu03

--- JS Question/Question03/question03.js
+++ JS Question/Question03/question03.js
@@ -1,2 +1,7 @@
 // 03. Declare a varibale and assign string value to it and change all the string characters to capital letters using 
 // toUpperCase() method.
+let uppercase="my name is bhagyashri fasale"
+console.log(uppercase.toUpperCase())//MY NAME IS BHAGYASHRI FASALE
+
+let lowercase="HELLO WORLD"
+console.log(lowercase.toLowerCase())//hello world
