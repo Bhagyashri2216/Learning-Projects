d4af3c94b0231d0a776e15ccadab271f09a3211b
Bhagyashri2216 <bhagyashrif1995@gmail.com>
2023-05-22 13:39:04 +0530

    solved qu05

--- JS Question/Question05/question05.js
+++ JS Question/Question05/question05.js
@@ -1,2 +1,5 @@
 // 05. Declare a varibale and assign string value to it and then split 
 // it into an array using split() method
+let array="MY name is raj"
+let result=array.split(" ")
+console.log(result)//[ 'MY', 'name', 'is', 'raj' ]
