3d6cae30ced551cb15bb76991f8328f810dbf31d
Bhagyashri2216 <bhagyashrif1995@gmail.com>
2023-07-06 14:30:03 +0530

    additiongame

--- project11_additiongame/script.js
+++ project11_additiongame/script.js
@@ -3,8 +3,8 @@ let secondNumber = document.getElementById("secondNumber");
 let userInput = document.getElementById("userInput");
 let gameResult = document.getElementById("gameResult");
 
-let successMessage="Congratulations!! You got it right";
-let tryAgainMessage="Please Try Again!";
+let successMessage="Congratulations!! You got it right";//result msg
+let tryAgainMessage="Please Try Again!";                  //result msg
 
 function restartButtonFunction() {
     let FirstRandomNumber = Math.random() * 100;
@@ -26,9 +26,13 @@ function checkButtonFunction() {
     let input = parseInt(userInput.value);
     let sumOfNumbers = FirstRandomNumber + secondRandomNumber;
     if (sumOfNumbers === input) {
-        gameResult.textContent = "Congratulations! You got it right.";
-        gameResult.style.backgroundColor = ""
+        gameResult.textContent = successMessage;
+        gameResult.style.backgroundColor="#028a0f";
 
+    }
+    else{
+        gameResult.textContent=tryAgainMessage;
+        gameResult.style.backgroundColor = "#1e217c";
     }
 
 }
